f156d426e4218febc75b64a44b7fd01bc2158421
0429oonishi <oonishireon110@gmail.com>
2021-07-28 23:32:11 +0900

    ViewModel作成

--- oonishiReonKadai12/ViewModels/ViewModel.swift
+++ oonishiReonKadai12/ViewModels/ViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol ViewModelInput {
+    
+}
+
+protocol ViewModelOutput: AnyObject {
+    
+}
+
+protocol ViewModelType {
+    var inputs: ViewModelInput { get }
+    var outputs: ViewModelOutput { get }
+}
+
+final class ViewModel: ViewModelInput, ViewModelOutput {
+    
+}
+
+extension ViewModel: ViewModelType {
+    
+    var inputs: ViewModelInput {
+        return self
+    }
+    
+    var outputs: ViewModelOutput {
+        return self
+    }
+    
+}
